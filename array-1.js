74d9a6e47067911675433db9476eda54c6af61ca
mgenova <marga.genova@gmail.com>
2016-04-26 12:11:35 -1000

    Exercise 1

---
+++ array-1.js
@@ -0,0 +1,29 @@
+// Create a grades object that stores a set of student grades in an object. Provide a function 
+// for adding a grade and a function for displaying the student's grade average.
+
+//Constructor
+function Grades() {
+    this.storeGrades = [];
+}
+
+
+// Adds a grade in the array
+Grades.prototype.add = function(grade) {
+    return this.storeGrades.push(grade);
+}
+
+// Averages grades in the array
+Grades.prototype.average = function() {
+   var total = 0;
+   this.storeGrades.forEach(function(grade) {
+   		total += grade;
+   });
+	return total/ this.storeGrades.length;
+}
+
+
+var grade = new Grades();
+
+console.log(grade.add(88));
+console.log(grade.average());
+
